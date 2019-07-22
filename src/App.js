989d6ea7d7cbfc712dfe5e81aaace26a3d867da5
brianbentancourt <brianbentancourt9@gmail.com>
2019-07-22 00:41:25 -0300

    Installing styled-components

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Category } from './components/category'
+
+export const App = () => (
+  <Category />
+)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { App } from './App'
 
-const app = document.getElementById("app")
+const app = document.getElementById('app')
 
-ReactDOM.render('Hola', app)
+ReactDOM.render(<App />, app)
