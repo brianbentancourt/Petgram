ef2c7e9469620ae2ce1f85a0b1f2326ed51c76dc
brianbentancourt <brianbentancourt9@gmail.com>
2019-07-23 21:23:06 -0300

    adding react-icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { ListOfCategories } from './components/listOfCategories'
 import { GlobalStyle } from './globalStyles'
+import { PhotoCard } from './components/photoCard'
 
 export const App = () => (
   <React.Fragment>
     <GlobalStyle />
     <ListOfCategories />
+    <PhotoCard />
   </React.Fragment>
 
 )
