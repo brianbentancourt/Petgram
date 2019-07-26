c72a558ab6dbac9966c94e1f350d74effa1dbd14
brianbentancourt <brianbentancourt9@gmail.com>
2019-07-25 22:40:56 -0300

    create logo and use hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { ListOfCategories } from './components/listOfCategories'
-import { GlobalStyle } from './globalStyles'
+import { GlobalStyle } from './styles/globalStyles'
 import { ListOfPhotoCards } from './components/listOfPhotoCard'
+import { Logo } from './components/logo'
 
 export const App = () => (
   <React.Fragment>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </React.Fragment>
