02cd2bb96b011a3c9ee8593a977bcc8fd980d14d
brianbentancourt <brianbentancourt9@gmail.com>
2019-08-01 01:36:06 -0300

    refactoring and using variables loading and error

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { ListOfCategories } from './components/listOfCategories'
 import { GlobalStyle } from './styles/globalStyles'
-import { ListOfPhotoCards } from './containers/ListOfPhotoCards'
+import { ListOfPhotoCardsWithQuery } from './containers/ListOfPhotoCardsWithQuery'
 import { Logo } from './components/logo'
+import { PhotoCardWithQuery } from './containers/PhotoCardWithQuery'
 
-export const App = () => (
-  <React.Fragment>
-    <GlobalStyle />
-    <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCards categoryId={2} />
-  </React.Fragment>
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
 
-)
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <Logo />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId} />
+          : <Fragment>
+            <ListOfCategories />
+            <ListOfPhotoCardsWithQuery categoryId={1} />
+          </Fragment>
+      }
+    </Fragment>
+  )
+}
