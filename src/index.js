635c751dae2cfb7060df515d9da738ec52f251c8
brianbentancourt <brianbentancourt9@gmail.com>
2019-08-01 00:48:56 -0300

    fetching photos using graphql (react-apollo, apollo-boost)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 
-const app = document.getElementById('app')
+const client = new ApolloClient({
+  uri: 'https://petgram-server.midudev.now.sh/graphql'
+})
 
-ReactDOM.render(<App />, app)
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('app'))
