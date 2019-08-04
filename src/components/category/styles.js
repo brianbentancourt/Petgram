a8be29b6aa35aa068f628a49b43de7f49c2cd8c3
brianbentancourt <brianbentancourt9@gmail.com>
2019-08-03 21:52:30 -0300

    Using Link to avoid reloading the page

--- src/components/category/styles.js
+++ src/components/category/styles.js
@@ -1,4 +1,13 @@
 import styled from 'styled-components'
+import { Link as LinkRouter } from '@reach/router'
+
+export const Link = styled(LinkRouter)`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  text-decoration: none;
+  width: 75px;
+`
 
 export const Anchor = styled.a`
   display: flex;
