4815f09b5588a27b8e334205ce9f4e6ca13b74ec
brianbentancourt <brianbentancourt9@gmail.com>
2019-08-03 22:12:07 -0300

    Detail page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Fragment } from 'react'
 import { GlobalStyle } from './styles/globalStyles'
 import { Logo } from './components/logo'
-import { PhotoCardWithQuery } from './containers/PhotoCardWithQuery'
 import { Home } from './pages/home'
 import { Router } from '@reach/router'
+import { Detail } from './pages/detail'
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
+  // const urlParams = new window.URLSearchParams(window.location.search)
+  // const detailId = urlParams.get('detail')
 
   return (
     <Fragment>
       <GlobalStyle />
       <Logo />
-      {
-        detailId
-          ? <PhotoCardWithQuery id={detailId} />
-          : <Router>
-            <Home path='/' />
-            <Home path='/pet/:id' />
-          </Router>
-      }
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:id' />
+        <Detail path='/detail/:detailId' />
+      </Router>
     </Fragment>
   )
 }
